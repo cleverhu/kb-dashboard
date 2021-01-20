8ed6d12d3f990ea31f7f8eecccda31028e83b321
deeplythink <hu928973510@gmail.com>
2021-01-20 14:02:20 +0800

    gormv2

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-
 import (
 	"fmt"
+	_ "knowledgeBase/src/common"
 	"knowledgeBase/src/models/DocGrpModel"
 	"knowledgeBase/src/models/DocModel"
 	"knowledgeBase/src/models/KbModel"
@@ -16,16 +16,15 @@ func main() {
 		Mutate(DocGrpModel.WithKbID(123))
 	fmt.Println(dpm)
 
-	dm:=DocModel.New().
-		Mutate(DocModel.WithDocID(1),DocModel.WithGroupID(123))
+	dm := DocModel.New().
+		Mutate(DocModel.WithDocID(1), DocModel.WithGroupID(123))
 	fmt.Println(dm)
 
-
-	kbm:=KbModel.New().
+	kbm := KbModel.New().
 		Mutate(KbModel.WithKbID(1))
 	fmt.Println(kbm)
 
-	kbum:=KbUserModel.New(KbUserModel.WithCanEdit("Y")).
+	kbum := KbUserModel.New(KbUserModel.WithCanEdit("Y")).
 		Mutate(KbUserModel.WithKbID(1))
 	fmt.Println(kbum)
 
